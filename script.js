30729e86eeddbc3b1e3aafae3db7463d767c96fd
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-09-01 20:57:32 +0200

    completato card body. lavoro su es js

---
+++ script.js
@@ -0,0 +1,20 @@
+const body= document.querySelector('body')
+const elements= document.querySelector('.container-fluid')
+
+
+window.onload= function(){
+    
+    
+    function smooth(){
+
+        element.scrollIntoView({behavior: 'smooth'})
+    }
+    
+}
+
+
+
+
+
+
+
